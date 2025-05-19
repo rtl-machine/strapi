e2d2a6cf1c8cded38dbc93aef66ff69a74fe5f32
Ivan Dončević Visković <ivan.doncevic@rtl.hr>
2025-05-19 17:11:16 +0200

    feat: plugin multiple users

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,24 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "ws:", "wss:", "http:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  'record-locking': {
+    enabled: true,
+  },
   "users-permissions": {
     config: {
       jwtSecret: env("JWT_SECRET"),
